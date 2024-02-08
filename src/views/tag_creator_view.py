b4e0473efbed779a8a32c20a9c8e254f5fad7762
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2024-02-07 21:48:48 -0300

    feat: implementando as regras de negócio

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,6 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
-
+from src.controllers.tag_creator_controller import TagCreatorController
 
 
 class TagCreatorView:
@@ -8,11 +8,14 @@ class TagCreatorView:
     # Responsabilidade para interagir com HTTP
     
     def validate_and_create(self, http_request: HttpRequest):
+        
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
 
         # Lógica de regra de negócio
-
+        formatted_response = tag_creator_controller.create(product_code)
         # Retorno de HTTP
 
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
